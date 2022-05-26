feeb2a3dfbc092604734141c5d42567a9340907e
Joss Sparkes <joss.sparkes@gmail.com>
2022-05-26 10:24:41 +0100

    Initial draft
    Signed-off-by: Joss Sparkes <joss.sparkes@gmail.com>

---
+++ tests/IntegrationTests/WorkflowManager.IntegrationTests/Features/WorkflowTaskArtefacts.feature
@@ -0,0 +1,41 @@
+﻿Feature: WorkflowTaskArtefact
+
+Artefacts can get passed into and between tasks
+
+@WorkflowTaskArtefacts
+Scenario Outline: Bucket exists in MinIO, publish workflow request which uses input artefacts
+    Given I have a bucket in MinIO dicom
+    And I have a clinical workflow Single_Task_Context_Input
+    When I publish a Workflow Request Message single_task_context_input
+    Then I can see task_output_destination Workflow Instance is created
+    And task_output_destination Task Dispatch event is published
+
+@WorkflowTaskArtefacts
+Scenario Outline: Bucket does not exist in MinIO, publish workflow request which uses non existant bucket
+    Given I have a clinical workflow 
+    When I publish a Workflow Request Message Bucket_Name
+    Then The workflow instance fails
+
+@WorkflowTaskArtefacts
+Scenario Outline: Create artefact in MinIO, publish task update message with artefact as output
+    Given I have a bucket in MinIO artefact
+    And I have a clinical workflow
+    And I publish a Workflow Request Message Bucket_Name
+    When I publish a task update message output_artefact
+    Then The workflow instance is updated with correct file path
+    And The task dispatch message is updated with correct file path
+
+@WorkflowTaskArtefacts
+Scenario Outline: Bucket exists in MinIO, send task dispatch with non existant file path
+    Given I have a bucket in MinIO dicom
+    And I have a clinical workflow
+    And I have a workflow instance
+    When I publish a task dispatch Message non_existant_filepath-required
+    Then The workflow instance fails
+
+@WorkflowTaskArtefacts
+Scenario Outline: Bucket exists in MinIO, send task dispatch with non existant artefact
+    Given I have a bucket in MinIO dicom
+    And I have a clinical workflow
+    When I publish a workflow instance non_existant_artefact
+    Then The workflow instance fails
---
+++ tests/IntegrationTests/WorkflowManager.IntegrationTests/StepDefinitions/WorkflowTaskArtefactStepDefinitions.cs
@@ -0,0 +1,63 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace Monai.Deploy.WorkflowManager.IntegrationTests.StepDefinitions
+{
+    [Binding]
+    public class WorkflowTaskArtefactStepDefinitions
+    {
+        [Given(@"I have a bucket in MinIO (.*)")]
+        public void GivenIHaveABucketInMinIO(string name)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I can see a task dispatch event with a path to the DICOM bucket")]
+        public void ThenICanSeeATaskDispatchEventWithAPathToTheDICOMBucket()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"The workflow instance fails")]
+        public void ThenTheWorkflowInstanceFails()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I publish a task update message (.*)")]
+        public void WhenIPublishATaskUpdateMessage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"The workflow instance is updated with correct file path")]
+        public void ThenTheWorkflowInstanceIsUpdatedWithCorrectFilePath()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"The task dispatch message is updated with correct file path")]
+        public void ThenTheTaskDispatchMessageIsUpdatedWithCorrectFilePath()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I have a workflow instance")]
+        public void GivenIHaveAWorkflowInstance()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I publish a task dispatch Message (.*)")]
+        public void WhenIPublishATaskDispatchMessage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I publish a workflow instance (.*)")]
+        public void WhenIPublishAWorkflowInstance()
+        {
+            throw new PendingStepException();
+        }
+    }
+}

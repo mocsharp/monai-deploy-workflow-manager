50edcd9b84025089cc861dd73dba1bc168252e85
Joe Batt <joe.batt@answerdigital.com>
2022-05-27 16:12:28 +0100

    update to integration tests
    Signed-off-by: Joe Batt <joe.batt@answerdigital.com>

--- tests/IntegrationTests/Monai.Deploy.WorkflowManager.TaskManager.IntegrationTests/Support/WebAppFactory.cs
+++ tests/IntegrationTests/Monai.Deploy.WorkflowManager.TaskManager.IntegrationTests/Support/WebAppFactory.cs
@@ -6,6 +6,13 @@ using Monai.Deploy.WorkflowManager.TaskManager.Argo;
 using Monai.Deploy.WorkflowManager.TaskManager.IntegrationTests.Support;
 using Monai.Deploy.WorkflowManager.TaskManager.Runner;
 using Microsoft.Extensions.DependencyInjection;
+using Monai.Deploy.WorkflowManager.Configuration;
+using Monai.Deploy.Storage.Configuration;
+using Monai.Deploy.Messaging.Configuration;
+using Monai.Deploy.Storage;
+using Monai.Deploy.Storage.MinIo;
+using Monai.Deploy.Messaging;
+using Monai.Deploy.Messaging.RabbitMq;
 
 namespace Monai.Deploy.WorkflowManager.TaskManager.IntegrationTests
 {
@@ -16,25 +23,38 @@ namespace Monai.Deploy.WorkflowManager.TaskManager.IntegrationTests
             var webApplicationFactory = new WebApplicationFactory<Program>();
             //var webApplicationFactory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
             //{
-            //    builder.ConfigureServices(services =>
+            //    builder.ConfigureServices((hostContext, services) =>
             //    {
-            //        services.AddSingleton<IArgoProvider, TestArgoProvider>();
-            //        services.AddSingleton<IKubernetesProvider, TestKubernetesProvider>();
+            //        services.AddOptions<WorkflowManagerOptions>().Bind(hostContext.Configuration.GetSection("WorkflowManager"));
+            //        services.AddOptions<StorageServiceConfiguration>().Bind(hostContext.Configuration.GetSection("WorkflowManager:storage"));
+            //        services.AddOptions<MessageBrokerServiceConfiguration>().Bind(hostContext.Configuration.GetSection("WorkflowManager:messaging"));
+
+            //        services.AddHttpClient();
+            //        services.UseRabbitMq();
+            //        services.AddSingleton<IStorageService, MinIoStorageService>();
+            //        services.AddSingleton<IMessageBrokerPublisherService, RabbitMqMessagePublisherService>();
+            //        services.AddSingleton<IMessageBrokerSubscriberService, RabbitMqMessageSubscriberService>();
+
+            //        services.AddSingleton<TaskManager>();
+            //        services.AddSingleton<IArgoProvider, ArgoProvider>();
+            //        services.AddSingleton<IKubernetesProvider, KubernetesProvider>();
+
+            //        services.AddHostedService<TaskManager>(p => p.GetRequiredService<TaskManager>());
             //    });
             //});
 
             _ = webApplicationFactory.CreateClient();
+
+            //public static async Task<HttpResponseMessage> GetConsumers()
+            //{
+            //    var httpClient = new HttpClient();
+
+            //    var svcCredentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(TestExecutionConfig.RabbitConfig.User + ":" + TestExecutionConfig.RabbitConfig.Password));
+
+            //    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", svcCredentials);
+
+            //    return await httpClient.GetAsync($"http://{TestExecutionConfig.RabbitConfig.Host}:{TestExecutionConfig.RabbitConfig.Port}/api/consumers");
+            //}
         }
-
-        //public static async Task<HttpResponseMessage> GetConsumers()
-        //{
-        //    var httpClient = new HttpClient();
-
-        //    var svcCredentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(TestExecutionConfig.RabbitConfig.User + ":" + TestExecutionConfig.RabbitConfig.Password));
-
-        //    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", svcCredentials);
-
-        //    return await httpClient.GetAsync($"http://{TestExecutionConfig.RabbitConfig.Host}:{TestExecutionConfig.RabbitConfig.Port}/api/consumers");
-        //}
     }
 }
